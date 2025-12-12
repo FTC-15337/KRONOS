ad4b0a5f6c7a7d8f965f4e46898d825c1163245e
Gahan Mummidi <gahanmummidi@gmail.com>
2025-12-12 12:27:03 -0800

    Tuned hood values with new smaller gear.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ShooterConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ShooterConfig.java
@@ -23,11 +23,11 @@ public class ShooterConfig {
         shooter.setVelocity(-280);
     }
     public void Stop() {shooter.setVelocity(0);}
-    public void hoodFar(){hood.setPosition(0.445);}
-    public void hoodMed(){hood.setPosition(0.5);}
-    public void hoodClose(){hood.setPosition(0.62);}
+    public void hoodFar(){hood.setPosition(0.57);}
+    public void hoodMed(){hood.setPosition(0.6);}
+    public void hoodClose(){hood.setPosition(0.8);}
     public void hoodZero(){hood.setPosition(0.0);}
-    public void hoodAutoFar(){hood.setPosition(0.44);}
+    public void hoodAutoFar(){hood.setPosition(0.57);}
     //0.63 for hood auto top
 
     public double velocityValue(){
